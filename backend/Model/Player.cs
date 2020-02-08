30eddb31208e5754e4cee77c2b86181585d58ad5
Cristi Mihai <cristi.mihai@gmail.com>
2020-02-08 13:30:17 +0000

    Revert "clean up"
    This reverts commit 9859f3d02474ec8669ae568043ade262d264cf0c.

--- backend/Model/Player.cs
+++ backend/Model/Player.cs
@@ -4,7 +4,7 @@ namespace IP5.Model
 	{
 		public string Code { get; set; }
 		public string Description { get; set; }
-		public string Email { get; set; }
+		public string? Email { get; set; }
 		public int Wins { get; set; }
 		public int Losses { get; set; }
 		public bool IsChampion { get; set; }
--- backend/Repositories/PlayersRepository.cs
+++ backend/Repositories/PlayersRepository.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using IP5.Model;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using IP5.Model;
+using System.Threading.Tasks;
 
 namespace IP5.Repositories
 {
@@ -12,34 +14,36 @@ namespace IP5.Repositories
         void Delete(string code);
     }
 
-    public class PlayersRepository : IPlayersRepository
+    public class PlayersRepository: IPlayersRepository
     {
         public List<Player> GetAll()
         {
-            return _data;
+            return this.data;
         }
 
         public Player Get(string code)
         {
-            return _data.First(i => i.Code == code);
+            return this.data.Find(i => i.Code == code);
         }
 
         public void Add(Player player)
         {
-            _data.Add(player);
+            this.data.Add(player);
         }
 
         public void Delete(string code)
         {
-            var index = _data.FindIndex(i => i.Code == code);
-            _data.RemoveAt(index);
+            var data = this.data;
+
+            var index = data.FindIndex(i => i.Code == code);
+            this.data.RemoveAt(index);
         }
 
-        private readonly List<Player> _data = new List<Player>()
-        {
-            new Player {Code = "jl", Description = "Joel", Wins = 3, Losses = 6},
-            new Player {Code = "jan", Description = "Jan", Wins = 5, Losses = 11, IsChampion = true},
-            new Player {Code = "andy", Description = "Andy", Wins = 11, Losses = 9}
-        };
-    }
+        protected List<Player> data = new List<Player>()
+            {
+                new Player {Code = "jl" , Description = "Joel", Wins = 3, Losses = 6},
+                new Player {Code = "jan", Description = "Jan", Wins = 5, Losses = 11, IsChampion = true},
+                new Player {Code = "andy" , Description = "Andy", Wins = 11, Losses = 9}
+            };
+        }
 }

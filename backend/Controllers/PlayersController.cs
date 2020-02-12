1326ebc49a25a08f1617bf7faeaa3dbfb3ca58d7
salgadoj <jose.salgado@iplicit.com>
2020-02-12 13:27:03 +0000

    Removed playersService for now

--- backend/Controllers/PlayersController.cs
+++ backend/Controllers/PlayersController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using IP5.Model;
-using IP5.Services;
+using IP5.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,38 +11,38 @@ namespace IP5.Controllers
 	[Route("[controller]")]
 	public class PlayersController : ControllerBase
 	{
-		private readonly IPlayersService _playerService;
+		private readonly IPlayersRepository _playerRepository;
 		private readonly ILogger<PlayersController> _logger;
 
-		public PlayersController(IPlayersService playerService, ILogger<PlayersController> logger)
+		public PlayersController(IPlayersRepository playersRepository, ILogger<PlayersController> logger)
 		{
-			_playerService = playerService;
+			_playerRepository = playersRepository;
 			_logger = logger;
 		}
 
 		[HttpGet]
 		public async IAsyncEnumerable<Player> GetAll()
 		{
-			await foreach (var player in _playerService.GetAll())
+			await foreach (var player in _playerRepository.GetAll())
 				yield return player;
 		}
 		
 		[HttpGet("{code}")]
 		public Task<Player> GetByCode([FromRoute] string code)
 		{
-			return _playerService.Get(code);
+			return _playerRepository.Get(code);
 		}
 
 		[HttpPut]
 		public void Create(Player player)
 		{
-			_playerService.Add(player);
+			_playerRepository.Add(player);
 		}
 
 		[HttpDelete]
 		public void Delete(string code)
 		{
-			_playerService.Delete(code);
+			_playerRepository.Delete(code);
 		}
 	}
 }

8ec7372e26c4ce94c994f58fd56d28eac4ea966c
salgadoj <jose.salgado@iplicit.com>
2020-02-14 15:16:07 +0000

    First (successful) test

--- frontend/src/views/players/players.ctrl.ts
+++ frontend/src/views/players/players.ctrl.ts
@@ -1,10 +1,10 @@
 class PlayersCtrl{
 
-    public getAll = async (baseUrl?: string): Promise<any> => {
-        return await (await fetch(baseUrl ? `${baseUrl}/api/players` : `/api/players`)).json();
-    }
+  public getAll = async (useProxy: boolean = true): Promise<any> => {
+    const url = useProxy ? `/api/players` : `http://localhost:5000/players`;
 
+    return await (await fetch(url)).json();
+  }
 }
-
 
 export const playersCtrl = new PlayersCtrl();
--- frontend/src/views/players/players.test.ts
+++ frontend/src/views/players/players.test.ts
@@ -3,9 +3,9 @@ import "isomorphic-fetch"
 // import {test, it} from "jest"
 
 describe("Testing players API", () => {
-    it('Get all players', async () => {
-        // expect.assertions(1);
-        const data = await playersCtrl.getAll("http://127.0.0.1:5000");
-        expect(data).not.toBeFalsy();
-    })
+  it('Get all players', async () => {
+    // expect.assertions(1);
+    const data = await playersCtrl.getAll(false);
+    expect(data).not.toBeFalsy();
+  })
 });
